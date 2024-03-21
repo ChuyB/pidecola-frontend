bfd6076996f8e6bc0c802221dfd7e2db6717d65f
ChuyB <bovea3359@gmail.com>
2024-03-21 18:00:02 -0400

    fix(landing): main div centers correctly on wide screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import HeroSection from "@/sections/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex flex-col grow items-center px-10 md:px-24 lg:px-32 xl:px-40 max-w-7xl">
-      <HeroSection />
-      <LogosSection />
+    <main className="flex flex-col justify-center items-center px-10 md:px-24 lg:px-32 xl:px-40">
+      <div className="max-w-7xl flex flex-col grow items-center">
+        <HeroSection />
+        <LogosSection />
+      </div>
     </main>
   );
 }
