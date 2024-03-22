05f4a26b1e2e118c2f18ccc1f04bf5ec2eaa1b94
KevinJOH <13-11007@usb.ve>
2024-03-21 20:25:33 -0400

    Falta conexion con el backend

--- src/components/ComentDislike.tsx
+++ src/components/ComentDislike.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Button } from "@nextui-org/react";
+import { Input } from "@nextui-org/input";
 
 const ComentDislike = () =>{
   const [coment, setComent] = useState("");
@@ -10,27 +11,37 @@ const ComentDislike = () =>{
     setComent(e.target.value);
   };
 
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsSend(true);
+    try {
+      //await fetch("Direccion de backend donde se envia el comentario");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
         <div className="mt-4">
-          <label htmlFor="comentDislike" className="mr-4 font-bold">Comentario: </label>
-          <input
-            type="text"
-            id="comentDislike"
-            value={coment}
-            onChange={handleComent}
-            className="rounded-md w-full"
-          />
+          <Input
+          type="textarea"
+          value={coment}
+          label = "Comentario"
+          labelPlacement="outside"
+          placeholder="¿Por qué califiacará negativo al conductor?"
+          onChange={handleComent}>
+          </Input>
         </div>
         <div className="flex justify-center items-center mt-4">
-        <Button color="primary" type="submit" className="shadow-lg shadow-slate-600"> Comentar </Button>
-      </div>
+          <Button 
+          color="primary" 
+          type="submit" 
+          className="shadow-lg shadow-slate-600"> 
+          Comentar 
+          </Button>
+        </div>
       </form>
       {isSend && 
       <div className="font-bold">
